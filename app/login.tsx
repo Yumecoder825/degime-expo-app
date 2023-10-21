8a99a374117a7c3841fbcab5f4414331314b1efe
better-think <web.solutions9218@gmail.com>
2023-10-21 16:06:32 +0800

    init

---
+++ app/login.tsx
@@ -0,0 +1,105 @@
+import { useState } from 'react';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import { Image } from 'expo-image';
+import { Entypo, MaterialCommunityIcons, SimpleLineIcons, Fontisto } from '@expo/vector-icons';
+
+import { Text, View } from '../components/Themed';
+import { InputGroup } from '../components/InputGroup';
+import { Button } from '../components/Button';
+
+export default function LoginScreen() {
+  const [isShowPasswordPreview, setIsShowPasswordPreview] = useState(false)
+
+  const handleOnPressViewPassword = () => {
+    setIsShowPasswordPreview(p => !p)
+  }
+
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require('../assets/images/degime-logo.png')} />
+      <View style={styles.pageContainer}>
+        <View style={styles.inputGroup}>
+          <Text style={styles.title}>ログイン</Text>
+          <Text style={styles.registerMessage}>アカウント登録がお済みでない方は、こちらから新規登録できます</Text>
+          <InputGroup
+            title='Eメール'
+            keyboardType='email-address'
+            leftIcon={<Fontisto name="email" size={20} color="black" />}
+            placeholder='ユーザー名かメールアドレスを入力してください'
+          />
+          <View>
+            <InputGroup
+              title='パスワード'
+              leftIcon={<SimpleLineIcons name="lock" size={20} color="black" />}
+              secureTextEntry={isShowPasswordPreview}
+              rightIcon={isShowPasswordPreview ? <MaterialCommunityIcons name="eye-off" size={24} color="black" /> : <Entypo name="eye" size={20} color="black" />}
+              onPressRightIcon={handleOnPressViewPassword}
+              placeholder='パスワードを入力してください'
+            />
+            <View style={styles.forgotPasswordButton}>
+              <TouchableOpacity>
+                <Text>パスワードを忘れた</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <Button color='white' bgColor='primary' text='ログイン' />
+        </View>
+        <View style={styles.socialIconGroup}>
+          <TouchableOpacity>
+            <Image style={styles.socialIcon} source={require('../assets/images/facebook-icon.png')} />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image style={styles.socialIcon} source={require('../assets/images/apple-icon.png')} />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image style={styles.socialIcon} source={require('../assets/images/google-icon.png')} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  pageContainer: {
+    marginTop: -30,
+    paddingLeft: 12,
+    paddingRight: 12,
+  },
+  inputGroup: {
+    flexDirection: 'column',
+    gap: 50
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    zIndex: 1
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  registerMessage: {
+    fontWeight: 'bold'
+  },
+  socialIconGroup: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    gap: 10,
+    padding: 20,
+  },
+  socialIcon: {
+    width: 30,
+    height: 30,
+    borderRadius: 999,
+  },
+  forgotPasswordButton: {
+    alignItems: 'flex-end',
+    marginTop: 12,
+    paddingRight: 30
+  }
+});
---
+++ components/Button.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import Colors from '../constants/Colors';
+import { Text, useThemeColor } from './Themed';
+
+
+type TButtonProps = {
+    bgColor: keyof typeof Colors.light & keyof typeof Colors.dark
+    color: keyof typeof Colors.light & keyof typeof Colors.dark
+    text: string;
+    rounded?: 'full';
+}
+export function Button({ color, bgColor, text, rounded = 'full' }: TButtonProps) {
+    const textColor = useThemeColor({}, color);
+    const backColor = useThemeColor({}, bgColor);
+
+    return (
+        <TouchableOpacity style={[styles.button, { backgroundColor: backColor, borderRadius: rounded === 'full' ? 999 : 0 }]}>
+            <Text style={[styles.text, { color: textColor }]}>{text}</Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    button: {
+        paddingTop: 16,
+        paddingBottom: 16,
+    },
+    text: {
+        textAlign: 'center',
+        fontSize: 15
+    }
+});
